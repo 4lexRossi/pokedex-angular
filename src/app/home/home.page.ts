1d99027c295f71880482b70e05a17ec4eaaeb0b0
4lexRossi <devalexrossi@gmail.com>
2020-09-04 23:25:36 -0300

    mostrando lista de pokemons index

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  offset = 0;
+  pokemon = [];
+  constructor(private pokeService: PokemonService) {}
 
-  constructor() {}
+  ngOnInit() {
+    this.loadPokemon();
+  }
+  loadPokemon(loadMore = false) {
+    if (loadMore) {
+      this.offset += 25;
+    }
+    this.pokeService.getPokemon(this.offset).subscribe(res => {
+      console.log('result: ', res);
+      if (this.offset > 0) {
+        this.pokemon = [...this.pokemon, ...res];
+      }else{
+        this.pokemon = res;
+      }
+    })
+  }
 
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +11,23 @@ export class PokemonService {
   imageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
   constructor(private http: HttpClient) { }
+
+  getPokemon(offset = 0) {
+    return this.http.get(`${this.baseUrl}/pokemon?offset=${offset}&limit=25`).pipe(
+      map(result => {
+        return result['results'];
+      }), map(pokemons => {
+        return pokemons.map((poke, index) => {
+          poke.image = this.getPokeImage(index + offset + 1);
+          poke.pokeIndex = offset + index + 1;
+          //adicionar os campos restantes aqui
+          return poke;
+        });
+      })
+    )
+  }
+
+  getPokeImage(index) {
+    return `${this.imageUrl}${index}.png`;
+  }
 }
